fa3c014b09436a0c037a4ff554fa8e650018ef6c
ejalice <ejalice123@naver.com>
2022-05-29 17:23:10 +0900

    Connect Firebase

--- TwiterSwiftUI/TwiterSwiftUI/TwiterSwiftUIApp.swift
+++ TwiterSwiftUI/TwiterSwiftUI/TwiterSwiftUIApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TwiterSwiftUIApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             LoginView()
